49f9a3ba7c2fae156972a9e21943cea967ec087d
Mariane Historia <144295541+mariane-historia@users.noreply.github.com>
2026-06-18 20:00:14 +0800

    feat: add viewport-fit=cover for safe-area-inset CSS support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./responsive.css";
 
@@ -7,6 +7,10 @@ export const metadata: Metadata = {
   icons: {
     icon: "/images/wkw_logo.png",
   },
+};
+
+export const viewport: Viewport = {
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
